aa54cc56845b788ef188ebf29794da497f841dcd
thawit <155454715+tacesept@users.noreply.github.com>
2026-04-20 09:56:43 +0700

    Add vitest for testing and implement cleanInput utility with corresponding tests

---
+++ src/test/cleanInput.test.ts
@@ -0,0 +1,29 @@
+import { describe, expect, test } from "vitest";
+import { cleanInput } from "../utils/cleanInput.js";
+
+describe.each([
+  {
+    input: "  ",
+    expected: [],
+  },
+  {
+    input: "  hello  ",
+    expected: ["hello"],
+  },
+  {
+    input: "  hello  world  ",
+    expected: ["hello", "world"],
+  },
+  {
+    input: "  HellO  World  ",
+    expected: ["hello", "world"],
+  },
+])("cleanInput($input)", ({ input, expected }) => {
+  test(`Expected: ${expected}`, () => {
+    const actual = cleanInput(input);
+    expect(actual).toHaveLength(expected.length);
+    for (const i in expected) {
+      expect(actual[i]).toBe(expected[i]);
+    }
+  });
+});

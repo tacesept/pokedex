ffc4d1aa8b61ef898f3c007f8ed67c5c01172cbc
thawit <155454715+tacesept@users.noreply.github.com>
2026-04-20 10:15:25 +0700

    Enhance main.ts with readline interface for user input, update tsconfig.json to include node types, and add repl.log to .gitignore

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,25 @@
+import readline from "node:readline";
+import { cleanInput } from "./utils/cleanInput.js";
+
 function main() {
-  console.log("Hello, world!");
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Pokedex > ",
+  });
+
+  rl.prompt();
+
+  rl.on("line", async (input) => {
+    const words = cleanInput(input);
+    if (words.length === 0) {
+      rl.prompt();
+      return;
+    }
+
+    console.log(`Your command was: ${words[0]}`);
+    rl.prompt();
+  });
 }
 
 main();
